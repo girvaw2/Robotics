cbe25f97a744091a9f28a53ee3029444ff2e8129
Billy <girvaw@gmail.com>
2011-10-19 20:48:55 +0100

    Bringing together ax12 controllers, head pointing and base-level project for opencv and pcl

---
+++ ros/maggie/phase1/head_tracking/build/CMakeFiles/head_tracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/head_tracking.dir/src/head_tracking.o"
+  "../bin/head_tracking.pdb"
+  "../bin/head_tracking"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/head_tracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
